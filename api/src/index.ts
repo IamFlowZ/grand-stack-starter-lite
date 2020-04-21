7d2297def6a994a4e298c5f69a08a9dd1640da03
iamflowz <dakota.lewallen17@gmail.com>
2020-04-21 13:13:02 -0400

    deps

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,39 @@
-console.log("hello there")
+import * as fs from "fs";
+import * as path from "path";
+
+import express from "express";
+import cors from "cors";
+import { express as voyagerMiddleware } from "graphql-voyager/middleware";
+import { ApolloServer, makeExecutableSchema } from "apollo-server-express";
+import { makeAugmentedSchema } from "neo4j-graphql-js";
+import neo4j from "neo4j-driver";
+
+// const resolvers = mySchema.resolvers
+const modifiedSchema = makeAugmentedSchema({
+  typeDefs: fs
+    .readFileSync(path.join(__dirname, "./schema.graphql"))
+    .toString(),
+  config: {
+    query: true, // default
+    mutation: false,
+  },
+});
+
+const driver = neo4j.driver(
+  "bolt://localhost:7687",
+  neo4j.auth.basic("neo4j", "letmein")
+);
+
+const server = new ApolloServer({
+  schema: modifiedSchema,
+  context: { driver },
+});
+
+const app = express();
+app.use(cors());
+app.use("/voyager", voyagerMiddleware({ endpointUrl: "/graphql" }));
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () => {
+  console.log("Listening on port 4000");
+});
